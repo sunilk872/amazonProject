c3f90c0b0067b497ac16466b5d3facf8b9365ebb
pranaykumarsamala <samalapranay02@gmail.com>
2024-09-26 23:22:02 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import { products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 
@@ -115,6 +116,8 @@ export function renderOrderSummary() {
             `.js-cart-item-container-${productId}`
          );
          container.remove(); 
+
+         renderPaymentSummary();
       });
 
    });
@@ -125,6 +128,7 @@ export function renderOrderSummary() {
          const {productId,deliveryOptionId}=element.dataset;
          updateDeliveryOption(productId,deliveryOptionId);
          renderOrderSummary();
+         renderPaymentSummary();
       });
    });
 }
